6a656e769b856b09b1dbf677df2692a66a94d785
Ikerm01 <ikermacias10@gmail.com>
2025-04-18 12:32:55 +0200

    update ex2

--- ex2.sh
+++ ex2.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+suma=0
+parells=0
+imparells=0
 
-numeros=$(for numero in {1..100}; do echo $RANDOM; done)
+numeros=$(for i in {1..100}; do echo $RANDOM; done)
+
+echo "$numeros" > numeros_aleatorios.txt
+
+echo "Numero Minimo:"
+sort -n numeros_aleatorios.txt | head -n1
+
+echo "Numero Maxim:"
+sort -n numeros_aleatorios.txt | tail -n1
+
+while read num; do
+  suma=$((suma + num))
+  if (( num % 2 == 0 )); then
+    parells=$((parells + 1))
+  else
+    imparells=$((imparells + 1))
+  fi
+done < numeros_aleatorios.txt
+
+mitjana=$((suma / 100))
+echo "Mitjana: $mitjana"
+echo "Parells: $parells"
+echo "Imparells: $imparells"
 
 
-echo "Numero Minimo"
-echo "$numeros" | sort -n | head -n1
-
-echo "Numero Maximo"
-echo "$numeros" | sort -n | tail -n1
